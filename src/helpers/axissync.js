d43bb14fd8e2be06ef6fd8f3e5d47172c4f50588
Alexander Morgushin <alexander.morgushin@devexpess.com>
2016-08-31 17:36:52 +0300

    Create helper for multuAxis tick syncronization.

---
+++ src/helpers/axissync.js
@@ -0,0 +1,70 @@
+import { TextSize } from "victory-core";
+import Axis from "src/helpers/axis";
+import { defaults } from "lodash";
+import * as d3Scale from "d3-scale";
+
+const defaultFontSize = 12;
+
+const getLongestString = (domains) => {
+  let longestString = "";
+  for (let domainsIndex = 0; domainsIndex < domains.length; domainsIndex++) {
+    for (let index = 0; index < domains[domainsIndex].length; index++) {
+      const element = String(domains[domainsIndex][index]);
+      if (element.length > longestString.length) {
+        longestString = String(element);
+      }
+    }
+  }
+  return longestString;
+};
+
+const getTextSize = (longestString, labelStyle) =>
+ TextSize.approximateTextSize(longestString, defaults(labelStyle, { fontSize: defaultFontSize }));
+
+const getExpectedLabelCount = (longestStringSize, axisProps) => {
+  const isVertical = Axis.isVertical(axisProps);
+  const size = (isVertical ? axisProps.height : axisProps.width) || 0;
+  const textSize = isVertical ? longestStringSize.height : longestStringSize.width;
+  return Math.floor(size / textSize);
+};
+
+const getTicks = (domain, tickCount) => {
+  const ticks = d3Scale.scaleLinear().domain(domain).range(0, 400).ticks(tickCount);
+  return ticks;
+};
+
+const syncTicks = (domainTicksArr) => {
+  const maxLength = Math.max.apply(null, domainTicksArr.map((arr) => arr.length));
+  return domainTicksArr.map((arr) => {
+    const needLength = maxLength - arr.length;
+    const ticks = arr.slice(0);
+    const tickInterval = arr[1] - arr[0];
+    for (let index = 0; index < needLength; index++) {
+      ticks.push(ticks[ticks.length - 1] + tickInterval);
+    }
+    return {
+      domain: [Math.min.apply(null, ticks), Math.max.apply(null, ticks)],
+      ticks
+    };
+  });
+};
+
+const sync = (domains, labelStyle, axisProps) => {
+  return syncTicks(domains.map((domain) => {
+    const labelCount = getExpectedLabelCount(
+       getTextSize(getLongestString(domain)),
+       axisProps
+    );
+    return getTicks(domain, labelCount);
+  }));
+};
+
+
+export default {
+  sync,
+  syncTicks,
+  getTicks,
+  getExpectedLabelCount,
+  getTextSize,
+  getLongestString
+};
---
+++ test/client/spec/helpers/axissync.spec.js
@@ -0,0 +1,65 @@
+/* eslint no-unused-expressions: 0 */
+
+import SyncAxis from "src/helpers/axissync";
+
+describe("helpers/axissync", () => {
+  describe("sync", () => {
+    it("return empty array with empty domains", () => {
+      expect(SyncAxis.sync([])).to.be.empty;
+    });
+  });
+  describe("getLongestString", () => {
+    it("return expected string", () => {
+      expect(SyncAxis.getLongestString([[124.12312, 3523], [121245, 23]])).to.be.eql("124.12312");
+    });
+  });
+  describe("getTextSize", () => {
+    it("return base font sizes without style", () => {
+      const size = SyncAxis.getTextSize("123");
+      expect(size.width.toFixed(2)).to.be.eql("20.76");
+      expect(size.height.toFixed(2)).to.be.eql("14.49");
+    });
+  });
+  describe("getExpectedLabelCount", () => {
+    it("return expected label count", () => {
+      const horizontalCount = SyncAxis.getExpectedLabelCount({width: 1, height: 1},
+        {orientation: "bottom", width: 2.99, height: 3.1}
+      );
+      const verticalCount =
+        SyncAxis.getExpectedLabelCount({width: 1, height: 1},
+        {orientation: "left", width: 2.99, height: 3.1}
+      );
+      expect(horizontalCount).to.be.eql(2);
+      expect(verticalCount).to.be.eql(3);
+    });
+  });
+  describe("extendDomainByTicksAndInterval", () => {
+    it("return expected ticks and interval", () => {
+      const ticks = SyncAxis.getTicks([0.1, 10], 5);
+      expect(ticks).to.be.eql([2, 4, 6, 8, 10]);
+    });
+  });
+  describe("syncTicks", () => {
+    it("return syncronized ticks", () => {
+      const syncronizedTicks = SyncAxis.syncTicks([
+         [2, 4, 6],
+         [3, 6, 9, 12]
+      ]);
+      expect(syncronizedTicks[0].ticks).to.be.eql([2, 4, 6, 8]);
+      expect(syncronizedTicks[1].ticks).to.be.eql([3, 6, 9, 12]);
+    });
+  });
+  describe("sync", () => {
+    it("return expected ticks and interval", () => {
+      const syncedAxises = SyncAxis.sync(
+          [[1, 15], [-1000, 4000]],
+          {},
+          {orientation: "left", height: 100}
+      );
+      expect(syncedAxises[0].ticks).to.be.eql([2, 4, 6, 8, 10, 12, 14]);
+      expect(syncedAxises[0].domain).to.be.eql([2, 14]);
+      expect(syncedAxises[1].ticks).to.be.eql([-1000, 0, 1000, 2000, 3000, 4000, 5000]);
+      expect(syncedAxises[1].domain).to.be.eql([-1000, 5000]);
+    });
+  });
+});
