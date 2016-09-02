1ccceb547710ba73a56c52407543636d038af9f5
Alexander Morgushin <alexander.morgushin@devexpess.com>
2016-09-02 11:21:01 +0300

    Change tickInterval in axis sync.

--- test/client/spec/helpers/axissync.spec.js
+++ test/client/spec/helpers/axissync.spec.js
@@ -1,8 +1,11 @@
 /* eslint no-unused-expressions: 0 */
-
+import * as d3Scale from "d3-scale";
+import * as d3 from "d3";
 import SyncAxis from "src/helpers/axissync";
 
 describe("helpers/axissync", () => {
+  // console.log(d3Scale.scaleTime().domain([new Date(2010, 1, 1),
+  //  new Date(2010, 2, 2)]).range([0, 400]).ticks(12));
   describe("sync", () => {
     it("return empty array with empty domains", () => {
       expect(SyncAxis.sync([])).to.be.empty;
@@ -35,15 +38,14 @@ describe("helpers/axissync", () => {
   });
   describe("extendDomainByTicksAndInterval", () => {
     it("return expected ticks and interval", () => {
-      const ticks = SyncAxis.getTicks([0.1, 10], 5);
-      expect(ticks).to.be.eql([2, 4, 6, 8, 10]);
+      expect(SyncAxis.getTicks([0.1, 10], 5).ticks).to.be.eql([2, 4, 6, 8, 10]);
     });
   });
   describe("syncTicks", () => {
     it("return syncronized ticks", () => {
       const syncronizedTicks = SyncAxis.syncTicks([
-         [2, 4, 6],
-         [3, 6, 9, 12]
+         {ticks: [2, 4, 6], tickInterval: 2},
+         {ticks: [3, 6, 9, 12], tickInterval: 3}
       ]);
       expect(syncronizedTicks[0].ticks).to.be.eql([2, 4, 6, 8]);
       expect(syncronizedTicks[1].ticks).to.be.eql([3, 6, 9, 12]);
