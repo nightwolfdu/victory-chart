58044a0355b6c5fca6498b805af75f5bae90aebd
Alexander Morgushin <alexander.morgushin@devexpess.com>
2016-09-05 19:34:56 +0300

    Use victory helper methods and refactor axissync for easy add new axis types.

--- src/helpers/axissync.js
+++ src/helpers/axissync.js
@@ -1,11 +1,19 @@
 import { TextSize } from "victory-core";
 import Axis from "./axis";
-import { defaults, range } from "lodash";
+import VictoryAxis from "../components/victory-axis/victory-axis.js";
+import AxisHelper from "../components/victory-axis/helper-methods.js";
+import { defaults, range, groupBy, flatten } from "lodash";
+import { Helpers } from "victory-core";
 import * as d3Scale from "d3-scale";
 import * as d3 from "d3";
 
 const defaultFontSize = 12;
-const defaultAxisProps = {orientation: "bottom", width: 0};
+
+const fallbackProps = {
+  width: 450,
+  height: 300,
+  padding: 50
+};
 
 const getShortestString = (domain) => {
   const domainStrs = d3.scaleLinear().domain(domain).nice().domain().map((elem) => String(elem));
@@ -15,42 +23,99 @@ const getShortestString = (domain) => {
 
 const getSize = (isVertical, sizeObj) => isVertical ? sizeObj.height : sizeObj.width;
 
-const getTicksAndInterval = (domain, axisRange, tickCount) =>
-  ({
+// const getTicksAndInterval = (domain, axisRange, tickCount) => ({
+//   ticks: d3Scale.scaleLinear().domain(domain).range(axisRange).ticks(tickCount),
+//   tickInterval: d3.tickStep(domain[0], domain[1], tickCount)
+// });
+// const dateSync = () => {
+//   const date2 = new Date(2016, 1, 1);
+//   const date = new Date(2015, 1, 1);
+//   console.log(date);
+//   console.log(date2);
+//   const diff = date2 - date;
+//   console.log(diff);
+//   console.log(diff / 5);
+//   console.log(new Date(date.getTime() + (diff / 5)));
+// }
+
+const getTicksAndInterval = (axisProps, isVertical, tickCount) => {
+  const domain = AxisHelper.getDomain(axisProps);
+  const axis = AxisHelper.getAxis(axisProps);
+  const axisRange = Helpers.getRange(axisProps, axis);
+  return {
     ticks: d3Scale.scaleLinear().domain(domain).range(axisRange).ticks(tickCount),
     tickInterval: d3.tickStep(domain[0], domain[1], tickCount)
-  });
-
-const syncTicks = (tickObject) => {
-  const maxLength = Math.max.apply(null, tickObject.map((obj) => obj.ticks.length));
-  return tickObject.map((obj) => {
-    return range(maxLength).map((index) => obj.ticks[0] + obj.tickInterval * (index));
-  });
+  };
 };
 
-const sync = (domains, labelStyle, axisProps) => {
-  const props = defaults(axisProps, defaultAxisProps);
-  const isVertical = Axis.isVertical(props);
-  const axisRange = getSize(isVertical, props);
-  const maxMinSize = Math.max.apply(null,
-    domains.map((rangeDomain) =>
+// const getTimeTicksAndInterval = (axisProps, isVertical, tickCount) => ({
+//   ticks: d3Scale.scaleTime().domain(axisProps.domain).range(getSize(isVertical, axisProps)).ticks(tickCount),
+//   tickInterval: d3.tickStep(axisProps.domain[0], axisProps.domain[1], tickCount)
+// });
+
+const syncTicks = (axisTicksArray) =>
+  axisTicksArray.map((obj) =>
+    ({
+      index: obj.index,
+      ticks: range(Math.max.apply(null, axisTicksArray.map((tickObj) => tickObj.ticks.length)))
+        .map((index) => obj.ticks[0] + obj.tickInterval * (index))
+    })
+  );
+
+
+const getLabelCount = (isVertical, props, size) =>
+  Math.max(Math.floor((getSize(isVertical, props)) / size), 2);
+
+const getTicksForAxisWithSync = (isVertical, axisGroup) => {
+  const maxMinSize = Math.max.apply(null, //Максимально-минимальный размер лейбла для расчета
+    axisGroup.map((obj) =>
       getSize(isVertical,
         TextSize.approximateTextSize(
-          getShortestString(rangeDomain), defaults(labelStyle, { fontSize: defaultFontSize })
+          getShortestString(obj.props.domain),
+          defaults(obj.props.style.tickLabels, { fontSize: defaultFontSize })
         )
       )
     )
   );
   return syncTicks(
-    domains.map((domain) => {
-      const labelCount = Math.max(
-        Math.floor(
-          (getSize(isVertical, props)) /
-          maxMinSize
-        ),
-      2);
-      return getTicksAndInterval(domain, axisRange, labelCount);
-    }));
+    axisGroup.map((axisObj) => {
+      return Object.assign(
+        getTicksAndInterval(
+          axisObj.props, isVertical, getLabelCount(isVertical, axisObj.props, maxMinSize)
+        ), { index: axisObj.index }
+      );
+    })
+  );
+};
+
+const getTicksWithoutSync = (isVertical, axisGroup) => //Все хорошо, но такая ось будет одна.
+  axisGroup.map((axisObj) => {
+    const minSize = getSize(isVertical, TextSize.approximateTextSize(
+      getShortestString(axisObj.props.domain),
+      defaults(axisObj.props.style.tickLabels, { fontSize: defaultFontSize })
+    ));
+    return Object.assign(
+      getTicksAndInterval(
+        axisObj.props, isVertical, getLabelCount(isVertical, axisObj.props, minSize)
+      ), { index: axisObj.index }
+     );
+  });
+
+const sync = (axisPropsArr) => {
+  console.log(getTimeTicksAndInterval({ domain: [new Date(2015, 1, 1), new Date(2016, 1, 1)], height: 100, orientation: "left" }, true, 12));
+  const modifyPropsArray = axisPropsArr.map((props) =>
+    defaults(props, VictoryAxis.defaultProps, fallbackProps)
+  );
+  const propsDict = modifyPropsArray.map((props, index) => ({index, props}));
+  const groupedAxis = groupBy(propsDict, (obj) => Axis.isVertical(obj.props));
+  const result = flatten(Object.keys(groupedAxis).map((key) => {
+    const isVertical = key;
+    const axisGroup = groupedAxis[isVertical];
+    return axisGroup.length > 1
+      ? getTicksForAxisWithSync(isVertical, axisGroup)
+      : getTicksWithoutSync(isVertical, axisGroup);
+  }));
+  return result.sort((obj1, obj2) => obj1.index > obj2.index).map((obj) => obj.ticks);
 };
 
 export default sync;
